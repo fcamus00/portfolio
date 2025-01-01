c07ed5817ee64bded0de9ad467657f421d5d2d57
Felipe Camus <felipe.camus.d@gmail.com>
2025-01-01 14:38:58 -0300

    added about section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,7 +1,94 @@
 import React from 'react';
+import Globe from 'react-globe.gl';
+import Button from '../components/Button';
 
 const About: React.FC = () => {
-  return <div>About</div>;
+  const [copied, setCopied] = React.useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText('felipe.camus.d@gmail.com');
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
+
+  return (
+    <section className="max-w-screen-2xl mx-auto c-space my-20">
+      <div className="grid xl:grid-cols-3 xl:grids-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img src="/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+            <div>
+              <p className="grid-headtext">Hi, I'm Felipe Camus</p>
+              <p className="grid-subtext">I'm a Fullstack Web Developer</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img src="/assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
+            <div>
+              <p className="grid-headtext">Tech Stack</p>
+              <p className="grid-subtext">I specialize in React Typescript and Next.js</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 xl:row-span-4">
+          <div className="grid-container">
+            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+              <Globe
+                height={326}
+                width={326}
+                backgroundColor="#010103"
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+              />
+            </div>
+            <div>
+              <p className="grid-headtext">I work remotely</p>
+              <p className="grid-subtext"> I'm based in Chile, with remote work available</p>
+              <Button name="Contact me" containerClass="w-full mt-10" isBeam />
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img src="/assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
+            <div>
+              <p className="grid-headtext">About me</p>
+              <p className="grid-subtext">
+                I'm a passionate and dedicated Fullstack Web Developer with a strong focus on building scalable and
+                maintainable web applications. I specialize in React, Typescript, and Next.js, and I'm always eager to
+                learn new technologies and improve my skills.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="/assets/grid4.png"
+              alt="grid-4"
+              className="w-full md:h-[276px] sm:h-[126px] h-fit object-cover object-top"
+            />
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">Contact Me</p>
+              <div className="copy-container" onClick={handleCopy}>
+                <img src={copied ? '/assets/tick.svg' : '/assets/copy.svg'} alt="copy" />
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">
+                  felipe.camus.d@gmail.com
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
